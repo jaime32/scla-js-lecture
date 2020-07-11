deca4c0587fd25a525e9151f2f678999eec8d8c9
TXT-50 <txt-50@MacBook-Pro-4.local>
2020-07-11 02:55:38 -0700

    scla js lecture

---
+++ js/index.js
@@ -0,0 +1,37 @@
+
+// controls for changing the background
+// create your event listener here
+// add the background image change code inside of the event listener
+
+
+
+
+// this is the code for changing a background image
+// missing image url
+// document.getElementById('background').style.backgroundImage = "url('')";
+
+
+
+
+
+
+
+
+// BUTTON CONTROLS
+
+// controls for the left button
+document.getElementById('left').addEventListener('click', function() {
+
+});
+
+// controls for the right button
+// missing element id and event type
+document.getElementById('').addEventListener('', function() {
+
+    var position = document.getElementById('ball').style.left;
+
+    var newPosition = parseInt(position) + 10;
+
+    document.getElementById('ball').style.left = newPosition + 'px';
+});
+
